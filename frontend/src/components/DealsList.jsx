4e734f9bc11a0c6bb92a0222a033a761c12193b8
Anders Nylund <anders.nylund.an@gmail.com>
2019-01-19 23:34:48 +0200

    Add deals list

--- frontend/src/components/DealsList.jsx
+++ frontend/src/components/DealsList.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Segment, Grid, Divider, Header, Image, Card } from 'semantic-ui-react';
 
 const GET_DEALS = gql`
   {
@@ -28,7 +29,60 @@ const GET_DEALS = gql`
 `;
 
 const DealsList = () => (
-  <Query query={GET_DEALS}>{({ data, loading, error }) => <Fragment />}</Query>
+  <Query query={GET_DEALS}>
+    {({ data, loading }) => {
+      if (loading) {
+        return <div>loading...</div>;
+      }
+      return (
+        <Fragment>
+          {data.myDeals.edges.map(deal => (
+            <Segment placeholder>
+              <Grid
+                columns={1}
+                stackable
+                verticalAlign="middle"
+                textAlign="center"
+              >
+                <Grid.Row verticalAlign="middle">
+                  <Grid.Column>
+                    <Card>
+                      <Image src={deal.myItem.image} />
+                      <Card.Content>
+                        <Card.Header>{deal.myItem.title}</Card.Header>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+
+                  <Divider horizontal>against</Divider>
+
+                  <Grid.Column>
+                    <Card>
+                      <Image src={deal.otherItem.image} />
+                      <Card.Content>
+                        <Card.Header>{deal.otherItem.title}</Card.Header>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+
+                  <Divider horizontal>with</Divider>
+
+                  <Grid.Column>
+                    <Header>
+                      {deal.participant.username}
+                      <Header.Subheader>
+                        {deal.participant.email}
+                      </Header.Subheader>
+                    </Header>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          ))}
+        </Fragment>
+      );
+    }}
+  </Query>
 );
 
 export default DealsList;
