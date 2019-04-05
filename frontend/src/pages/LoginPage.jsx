80be27c3873f1c53d09bc65ab1d1c4170edb542f
Anders Nylund <anders.nylund.an@gmail.com>
2019-04-05 17:57:09 +0300

    Add info to LoginPage about email and password

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -42,9 +42,10 @@ class Login extends React.Component {
       <Fragment>
         <Header as="h2">Welcome to handel</Header>
         <Message color="red">
-          This is a testing version of the early version of the service. I take
-          no responsibility or liability of anything you do with the service. I
-          may delete all data at any point.
+          DO NOT USE A REAL EMAIL OR PASSWORD! <br /> This is a testing version
+          of the early version of the service. I take no responsibility or
+          liability of anything you do with the service. I may delete all data
+          at any point.
         </Message>
         <Form>
           <Form.Field>
